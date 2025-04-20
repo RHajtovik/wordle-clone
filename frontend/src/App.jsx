2de7721708aa722c0f52175939120bebb653eb0f
RHajtovik <ryanh2135@gmail.com>
2025-04-20 05:17:06 -0400

    switched backend to run on render, beanstalk wasnt working right

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   const [invalidText, setInvalidText] = useState('');
 
   // Backend URL
-  const BASE_URL = 'https://wordle-backend-env-2.eba-stm2gqev.us-east-2.elasticbeanstalk.com';
+  const BASE_URL = 'https://wordle-clone-moer.onrender.com/';
 
   const initializeGameState = () => {
     setGuesses(Array(6).fill(''));
